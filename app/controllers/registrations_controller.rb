6dcd9cdc8ec6cc9d4661a536cd8465134028df04
nick <nicholasdipreta2020@gmail.com>
2020-08-18 13:20:24 -0400

    fixed controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,19 +1,18 @@
 class RegistrationsController < ApplicationController
   def create
     user = User.create!(
-      email: params["user"]["email"],
-      password: params["user"]["password"],
-      password_confirmation: params["user"]["password_confirmation"]
+      email: params["email"],
+      password: params["password"],
+      password_confirmation: params["password_confirmation"],
     )
 
     if user
       session[:user_id] = user.id
       render json: {
-               user: user
+               user: user,
              }
     else
       render json: { status: 500 }
     end
   end
 end
-
